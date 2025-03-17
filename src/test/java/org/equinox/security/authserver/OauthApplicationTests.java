0fef6ccc67bb84d697a8f4c522591eab9e3cb298
equinox766 <Equinox766@gmail.com>
2025-03-17 15:28:27 -0300

    first commit

---
+++ src/test/java/org/equinox/security/authserver/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.equinox.security.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
